ad483b86914775e0f7aeb20d8cadb810ebbda58e
ard4b <53410100+ard4b@users.noreply.github.com>
2022-01-13 01:52:46 +0300

    Add files via upload

---
+++ Csharp_Patika/Csharp_Patika/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Patika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
