b42505cee90482049e58d86320a4fe061aa1e57f
ard4b <53410100+ard4b@users.noreply.github.com>
2022-01-13 19:45:41 +0300

    Add files via upload

---
+++ Csharp_Patika_Operatorler/Csharp_Patika_Operatorler/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Csharp_Patika_Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atama ve işlemli atama
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            y += 5;
+
+            Console.WriteLine(y += 10);
+
+            Console.WriteLine(y -= 5);
+
+            Console.WriteLine(y *= 2);
+
+            Console.WriteLine(y /= 3);
+
+            //Mantıksal Operatörler
+            // ||(veya), &&(ve), !(Değil)
+
+            bool isSuccess = true;
+            bool isComleted = false;
+
+
+            if (isSuccess && isComleted)
+                Console.WriteLine("perfect!");
+
+            if (isSuccess || isComleted)
+                Console.WriteLine("Great!");
+
+            if (isSuccess && ! isComleted)
+                Console.WriteLine("FİNE!");
+
+            //İlişkisel Operatörler
+            // <,>,<=,>=,==,!=
+
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+
+            // % mod alır
+            int sonuc2 = 2354;
+            Console.WriteLine(sonuc2);
+
+
+
+
+
+
+
+
+        }
+    }
+}
